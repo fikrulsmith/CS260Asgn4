a6b3279250758c6301b1c4af35c83ec09c866aeb
madguy187 <nubbiehood@hotmail.com>
2021-07-29 14:39:49 +0800

    added pch

--- A4LANGame/Client/Client.h
+++ A4LANGame/Client/Client.h
@@ -1,16 +1,5 @@
 #pragma once
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#include "Windows.h"		// Entire Win32 API...
-// #include "winsock2.h"	// ...or Winsock alone
-#include "ws2tcpip.h"		// getaddrinfo()
-
-#include <vector>
-
-#include "ClientInfo.h"
+#include "../Client/ClientInfo.h"
 
 class Client
 {
--- A4LANGame/Client/ClientSender/ClientSender.cpp
+++ A4LANGame/Client/ClientSender/ClientSender.cpp
@@ -1,17 +1,18 @@
+#include "pch.h"
 #include "ClientSender.h"
 
-//int ClientSender::SendClient(ClientInfo info, std::string message)
-//{
-//	int bytesSend = sendto(info.socket, message.c_str(),
-//		static_cast<int>(message.length()), 0, info.addr->ai_addr, sizeof(*info.addr->ai_addr));
-//
-//	if (bytesSend == SOCKET_ERROR)
-//	{
-//		size_t errorCode = WSAGetLastError();
-//		std::cout << errorCode << std::endl;
-//		std::cerr << "send failed" << std::endl;
-//		return bytesSend;
-//	}
-//
-//	return bytesSend;
-//}
+int ClientSender::SendClient(ClientInfo info, std::string message)
+{
+	int bytesSend = sendto(info.socket, message.c_str(),
+		static_cast<int>(message.length()), 0, info.addr->ai_addr, sizeof(*info.addr->ai_addr));
+
+	if (bytesSend == SOCKET_ERROR)
+	{
+		size_t errorCode = WSAGetLastError();
+		std::cout << errorCode << std::endl;
+		std::cerr << "send failed" << std::endl;
+		return bytesSend;
+	}
+
+	return bytesSend;
+}
