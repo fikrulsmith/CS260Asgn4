69b724a911e93c83ee8dabaf02155d359ef6e868
ghthegreat <56424881+ghthegreat@users.noreply.github.com>
2021-08-08 21:24:23 +0800

    can edi

--- A4LANGame/Src/Main.cpp
+++ A4LANGame/Src/Main.cpp
@@ -73,9 +73,12 @@ int WINAPI WinMain(_In_ HINSTANCE instanceH, _In_opt_ HINSTANCE prevInstanceH, _
 		while(GSManager->GetGameStateCurrIndex() == GSManager->GetGameStateNextIndex())
 		{
 			AESysFrameStart();
-			client->UpdateAllDeadReckoningDT(g_dt);
+			if(GSManager->GetGameStateCurrIndex() == GS_ASTEROIDS)
+				client->UpdateAllDeadReckoningDT(g_dt);
 			//checking of recv
-			client->AllDeadReckoningCorrection(g_dt);
+
+			if (GSManager->GetGameStateCurrIndex() == GS_ASTEROIDS)
+				client->AllDeadReckoningCorrection(g_dt);
 
 			AEInputUpdate();
 			GSManager->GameStateUpdate();
