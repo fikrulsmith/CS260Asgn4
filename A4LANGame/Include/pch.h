443434a6c667f4a6859204d68ffec947e7651901
madguy187 <nubbiehood@hotmail.com>
2021-08-11 01:14:02 +0800

    commented pch

--- A4LANGame/Include/pch.h
+++ A4LANGame/Include/pch.h
@@ -1,3 +1,17 @@
+/******************************************************************************/
+/*!
+\file pch.h
+\author Wong Swee Jong Nico
+\par email: s.wong\@digipen.edu
+\par DigiPen login: s.wong
+\par Course: CS260-B
+\par Assignment #04
+\date 10/08/2021
+\brief
+This file contains an implementation of the functionality of a precompiled
+header
+*/
+/******************************************************************************/
 #pragma once
 #pragma comment(lib, "ws2_32.lib")
 
